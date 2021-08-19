4bd35fcfe18d0b71d1233e17af039747e3339c8e
André Næss <andre.naess@gmail.com>
2021-08-19 22:46:06 +0200

    Starting chapter 4

---
+++ code/part04/language/src/main/java/toyl/ast/ToylSubNode.java
@@ -0,0 +1,17 @@
+package toyl.ast;
+
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.Specialization;
+
+@NodeChild("left") @NodeChild("right")
+public abstract class ToylSubNode extends ToylNode {
+  @Specialization(rewriteOn = ArithmeticException.class)
+  protected int subInts(int leftValue, int rightValue) {
+    return Math.subtractExact(leftValue, rightValue);
+  }
+
+  @Specialization(replaces = "subInts")
+  protected double subDoubles(double leftValue, double rightValue) {
+    return leftValue - rightValue;
+  }
+}
---
+++ code/part04/language/src/main/java/toyl/parser/ToylParseTreeVisitor.java
@@ -0,0 +1,38 @@
+package toyl.parser;
+
+import toyl.ast.*;
+
+import java.math.BigDecimal;
+
+public class ToylParseTreeVisitor extends ToylBaseVisitor<ToylNode> {
+  @Override
+  public ToylNode visitProgram(ToylParser.ProgramContext ctx) {
+    return this.visit(ctx.expr());
+  }
+
+  @Override
+  public ToylNode visitParenthesizedExpr(ToylParser.ParenthesizedExprContext ctx) {
+    return this.visit(ctx.expr());
+  }
+
+  @Override
+  public ToylNode visitArithmeticExpression(ToylParser.ArithmeticExpressionContext ctx) {
+    return switch (ctx.binaryOp.getText()) {
+      case "+" -> ToylAddNodeGen.create(this.visit(ctx.left), this.visit(ctx.right));
+      case "-" -> ToylSubNodeGen.create(this.visit(ctx.left), this.visit(ctx.right));
+      case "/" -> ToylDivNodeGen.create(this.visit(ctx.left), this.visit(ctx.right));
+      case "*" -> ToylMulNodeGen.create(this.visit(ctx.left), this.visit(ctx.right));
+      default -> throw new IllegalStateException("Unexpected arithmetic operator: " + ctx.binaryOp.getText());
+    };
+  }
+
+  @Override
+  public ToylNode visitLiteralNumber(ToylParser.LiteralNumberContext ctx) {
+    var number = new BigDecimal(ctx.LITERAL_NUMBER().getText());
+    if (number.scale() > 0 || number.compareTo(BigDecimal.valueOf(Integer.MAX_VALUE)) > 0) {
+      return new ToylLiteralDoubleNode(number.doubleValue());
+    } else {
+      return new ToylLiteralIntNode(number.intValue());
+    }
+  }
+}
